32a01964891245c0ab6fd49f1c3f73dbb399f291
y20k <mail@y20k.org>
2019-10-09 16:41:50 +0200

    version number incremented

--- app/src/main/java/org/y20k/escapepod/helpers/LogHelper.kt
+++ app/src/main/java/org/y20k/escapepod/helpers/LogHelper.kt
@@ -30,7 +30,7 @@ object LogHelper {
     private const val TESTING: Boolean = false // set to "false"
     private const val LOG_PREFIX: String = "escapepod_"
     private const val MAX_LOG_TAG_LENGTH: Int = 64
-    private val LOG_PREFIX_LENGTH: Int = LOG_PREFIX.length
+    private const val LOG_PREFIX_LENGTH: Int = LOG_PREFIX.length
 
     fun makeLogTag(str: String): String {
         return if (str.length > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
