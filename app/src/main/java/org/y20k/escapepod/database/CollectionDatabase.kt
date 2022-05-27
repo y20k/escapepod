afa3d9df2db545f37b61a0027bbef59fdd22a91a
y20k <mail@y20k.org>
2022-05-27 23:32:12 +0200

    room - migration of the db scheme (now works with older versions of Android)

--- app/src/main/java/org/y20k/escapepod/database/CollectionDatabase.kt
+++ app/src/main/java/org/y20k/escapepod/database/CollectionDatabase.kt
@@ -15,11 +15,8 @@
 package org.y20k.escapepod.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import androidx.room.migration.Migration
+import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
 import androidx.sqlite.db.SupportSQLiteDatabase
 import org.y20k.escapepod.database.daos.EpisodeDao
 import org.y20k.escapepod.database.daos.EpisodeDescriptionDao
@@ -35,7 +32,7 @@ import org.y20k.escapepod.database.wrappers.EpisodeMostRecentView
 /*
  * CollectionDatabase class
  */
-@Database(version = 2, entities = [Podcast::class, PodcastDescription::class, Episode::class, EpisodeDescription::class], views = [EpisodeMostRecentView::class])
+@Database(version = 2, entities = [Podcast::class, PodcastDescription::class, Episode::class, EpisodeDescription::class], views = [EpisodeMostRecentView::class], autoMigrations = [AutoMigration (from = 1, to = 2, spec = CollectionDatabase.CustomMigrationSpec::class)], exportSchema = true)
 @TypeConverters(Converters::class)
 abstract class CollectionDatabase : RoomDatabase() {
 
@@ -47,31 +44,28 @@ abstract class CollectionDatabase : RoomDatabase() {
 
     abstract fun episodeDescriptionDao(): EpisodeDescriptionDao
 
+    /* Specifies the database changes from version 1 to version 2 */
+    @RenameColumn(fromColumnName = "playback_state", toColumnName = "is_playing", tableName = "episodes")
+    class CustomMigrationSpec: AutoMigrationSpec {
+        override fun onPostMigrate(db: SupportSQLiteDatabase) {
+            super.onPostMigrate(db)
+            db.execSQL("UPDATE episodes SET is_playing = 0")
+        }
+    }
 
     /* Object used to create an offer an instance of the collection database */
     companion object {
 
         var INSTANCE: CollectionDatabase? = null
 
-        val migration_1_2: Migration = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // todo migration only works for API 30
-                // todo: reason RENAME COLUMN is only supported from SQLITE v3.25.0 on
-                // todo see also https://developer.android.com/reference/android/database/sqlite/package-summary
-                database.execSQL("ALTER TABLE episodes RENAME COLUMN playback_state TO is_playing")
-                database.execSQL("UPDATE episodes SET is_playing = 0")
-            }
-        }
-
         fun getInstance(context: Context): CollectionDatabase {
             synchronized(this) {
                 var instance: CollectionDatabase? = INSTANCE
 
                 if (instance == null) {
-                    instance = Room.databaseBuilder(context.applicationContext, CollectionDatabase::class.java, "collection_database")
-                        .addMigrations(migration_1_2)
-                        .fallbackToDestructiveMigration()
-                        .build()
+                    instance = Room.databaseBuilder(context.applicationContext, CollectionDatabase::class.java, "collection_database").apply {
+                        fallbackToDestructiveMigration()
+                    }.build()
                     INSTANCE = instance
                 }
                 return instance
